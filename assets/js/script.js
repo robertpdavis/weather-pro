fbc109920b6af041f1a216971c605e7836afed25
Buxta <robertpdavis@optusnet.com.au>
2022-03-26 15:57:51 +1000

    Further desgn HTML and CSS and inital script coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,48 @@
 // Weather Pro JS
+
+//DOM vars
+var formElement = $("#search-form");
+var cityCard = $("#city-card");
+
+console.log(cityCard);
+
+
+//Listeners
+formElement.on('click', 'button', handleFormClick);
+
+
+//Functions
+
+function handleFormClick (event) {
+    event.preventDefault();
+    var optionValue = event.target.value;
+    var searchString;
+
+    if (optionValue != "" && optionValue != null && typeof optionValue === "string") {
+
+        if (optionValue === "Search") {          
+            searchString = formElement[0][0].value;
+        } else {
+            searchString = optionValue;
+        }
+
+        if (searchString != "" && searchString != null && typeof searchString === "string") {
+            searchApi(searchString);
+        }
+    }
+}
+
+function searchApi(searchString) {
+    console.log(searchString);
+
+}
+
+
+function init () {
+
+}
+
+
+$(function () {
+    init()
+});
